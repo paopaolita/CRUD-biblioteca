f800b9f4f9d502fe7f14773ea79d018ef7396f33
paopaolita <132370804+paopaolita@users.noreply.github.com>
2023-05-02 08:40:20 -0600

    Bbiblioteca
    Aqui se subio el crud de base de datos, y la

---
+++ Base de datos/Base de datos/Form2.vb
@@ -0,0 +1,23 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class Form2
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim cstring As String
+        cstring = "server=localhost;user=root;database=biblioteca;port=3306;password=CVO2023;"
+        Dim conn As New MySqlConnection(cstring)
+        Try
+            Dim sQuery As String
+            sQuery = "SELECT * FROM biblioteca.libro;"
+            Dim da As New MySqlDataAdapter(sQuery, conn)
+            Dim dt As New DataTable
+            da.Fill(dt)
+            DataGridView1.DataSource = dt
+            DataGridView1.Refresh()
+            conn.Close()
+
+
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message.ToString())
+        End Try
+    End Sub
+End Class
---
+++ Base de datos/Base de datos/Menu.Designer.vb
@@ -0,0 +1,67 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Menu
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Menu))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.MediumTurquoise
+        Me.Button1.Location = New System.Drawing.Point(104, 48)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(149, 110)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "autores"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.MediumTurquoise
+        Me.Button2.Location = New System.Drawing.Point(104, 184)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(149, 110)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "libros"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Menu
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.PaleTurquoise
+        Me.ClientSize = New System.Drawing.Size(358, 339)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Menu"
+        Me.Text = "Menu"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+End Class
